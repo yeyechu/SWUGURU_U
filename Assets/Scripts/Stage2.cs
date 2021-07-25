1009ee52d33f0fea844002d6dc87076ef0e5571b
yelim320 <yelimpark320@gmail.com>
2021-07-25 23:00:48 +0900

    스테이지2 미니게임 구현

---
+++ Assets/Scripts/Stage2.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage2 : MonoBehaviour
+{
+    public GameObject point;
+    public float distanceToPoint;
+    public float successRange = 10f;
+
+    public float t = 1f; // speed
+    public float l = 10f; // length from 0 to endpoint.
+    public float posX = 1f; // Offset
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        point = GameObject.Find("Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = new Vector3(posX + Mathf.PingPong(t * Time.time, l), 0, 0);
+        transform.position = pos;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            distanceToPoint = Vector3.Distance(transform.position, point.transform.position);
+
+            if(distanceToPoint <= successRange)
+            {
+                print("성공");
+            }
+            else
+            {
+                print("실패");
+            }
+        }
+    }
+}
